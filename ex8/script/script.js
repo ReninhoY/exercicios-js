6efda555811ff70264f2f4f77a690fdf2c4fb20e
ReninhoY <101796281+ReninhoY@users.noreply.github.com>
2022-11-30 23:41:32 -0300

    Add files via upload

---
+++ ex8/script/script.js
@@ -0,0 +1,35 @@
+function ex1() {
+    let ex1 = "../ex1.html"
+    document.getElementById("pag").src=ex1
+    document.getElementById("enunciado").innerText="Desenvolver um programa (Web) para exibir todos os números pares de um intervalo de 1 a 5000."
+}
+function ex2() {
+    let ex2 = "../ex2.html"
+    document.getElementById("pag").src=ex2
+    document.getElementById("enunciado").innerText="Desenvolva uma página que leia uma temperatura e converta a ela para os graus Fahrenheit ou Celsius."
+}
+function ex3() {
+    let ex3 = "../ex3.html"
+    document.getElementById("pag").src=ex3
+    document.getElementById("enunciado").innerText="Desenvolva uma página leia o nome, o valor e a quantidade vendida de três produtos. Calcule o valor total da venda."
+}
+function ex4() {
+    let ex4 = "../ex4.html"
+    document.getElementById("pag").src=ex4
+    document.getElementById("enunciado").innerText="Desenvolva uma página que leia a quilometragem percorrida por um determinado veículo e o valor do combustível. Calcule o gasto dele, sabendo que o veículo percorre 8km/litro."
+}
+function ex5() {
+    let ex5 = "../ex5.html"
+    document.getElementById("pag").src=ex5
+    document.getElementById("enunciado").innerText="Desenvolva uma página para calcular o custo para assentar piso, sabendo que o preço é de 36,00 por m². O programa deve ler o comprimento e a largura total do cômodo."
+}
+function ex6() {
+    let ex6 = "../ex6.html"
+    document.getElementById("pag").src=ex6
+    document.getElementById("enunciado").innerText="Tendo como dados de entrada a altura e o sexo de uma pessoa a partir de uma caixa de combinação, construa uma página que calcule seu peso ideal, utilizando as seguintes fórmulas: Para homens: (72.7*h) - 58 Para mulheres: (62.1*h) - 44.7 (h = altura)"
+}
+function ex7() {
+    let ex7 = "../ex7.html"
+    document.getElementById("pag").src=ex7
+    document.getElementById("enunciado").innerText="Crie uma página que receba N valores do usuário. Crie botões para mostrar todos os valores inseridos, o maior valor e o menor valor do vetor."
+}
